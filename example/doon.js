6feaa06c9f7c508b40eef7c7fc792b1d65f59b3e
Christian Blanquera <cblanquera@openovate.com>
2018-03-08 01:29:51 +0800

    deploying to yarn

--- example/doon.js
+++ example/doon.js
@@ -7,38 +7,38 @@
  * @license MIT
  */
 jQuery.fn.extend({
-	doon: function() {
-		return this.each(function() {
-			jQuery('*[data-do]', this).add(this)
-			.each(function() {
-				var trigger = $(this);
+    doon: function() {
+        return this.each(function() {
+            jQuery('*[data-do]', this).add(this)
+            .each(function() {
+                var trigger = $(this);
 
-				var action = trigger.attr('data-do');
-				
-				if(!action || trigger.data('doon')) {
-					return;
-				}
-				
-				trigger.data('doon', true);
-				
-				var event = trigger.attr('data-on');
-	
-				//trigger init
-				jQuery(window).trigger(action+'-init', [this]);
-	
-				if(!event) {
-					return;
-				}
-	
-				jQuery.each(event.split('|'), function(i, event) {
-					trigger.on(event, function(e) {
-						//mod the custom event type
-						e.type = action+'-'+event;
-						//pass it along
-						$(window).trigger(e, [this]);
-					});
-				});
-			});
-		});
-	}
+                var action = trigger.attr('data-do');
+
+                if(!action || trigger.data('doon')) {
+                    return;
+                }
+
+                trigger.data('doon', true);
+
+                var event = trigger.attr('data-on');
+
+                //trigger init
+                jQuery(window).trigger(action+'-init', [this]);
+
+                if(!event) {
+                    return;
+                }
+
+                jQuery.each(event.split('|'), function(i, event) {
+                    trigger.on(event, function(e) {
+                        //mod the custom event type
+                        e.type = action+'-'+event;
+                        //pass it along
+                        $(window).trigger(e, [this]);
+                    });
+                });
+            });
+        });
+    }
 });
