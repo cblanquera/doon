5533cf85c4aae8e8593f809cc43de70fb0742090
Christian Blanquera <cblanquera@openovate.com>
2020-05-22 18:00:51 +0800

    allowing an option to retarget the doon

--- doon.js
+++ doon.js
@@ -7,38 +7,43 @@
  * @license MIT
  */
 jQuery.fn.extend({
-    doon: function() {
-        return this.each(function() {
-            jQuery('*[data-do]', this).add(this)
-            .each(function() {
-                var trigger = $(this);
+  doon: function() {
+    return this.each(function() {
+      jQuery('*[data-do]', this).add(this)
+      .each(function() {
+        var trigger = jQuery(this);
 
-                var action = trigger.attr('data-do');
+        var action = trigger.attr('data-do');
 
-                if(!action || trigger.data('doon')) {
-                    return;
-                }
+        if(!action || trigger.data('doon')) {
+          return;
+        }
 
-                trigger.data('doon', true);
+        trigger.data('doon', true);
 
-                var event = trigger.attr('data-on');
+        var event = trigger.attr('data-on');
 
-                //trigger init
-                jQuery(window).trigger(action+'-init', [this]);
+        var target = this;
+        if (trigger.data('doon-target')) {
+          target = jQuery(trigger.data('doon-target'));
+        }
 
-                if(!event) {
-                    return;
-                }
+        //trigger init
+        jQuery(window).trigger(action+'-init', [target]);
 
-                jQuery.each(event.split('|'), function(i, event) {
-                    trigger.on(event, function(e) {
-                        //mod the custom event type
-                        e.type = action+'-'+event;
-                        //pass it along
-                        $(window).trigger(e, [this]);
-                    });
-                });
-            });
+        if(!event) {
+          return;
+        }
+
+        jQuery.each(event.split('|'), function(i, event) {
+          jQuery(target).on(event, function(e) {
+            //mod the custom event type
+            e.type = action+'-'+event;
+            //pass it along
+            jQuery(window).trigger(e, [target]);
+          });
         });
-    }
+      });
+    });
+  }
 });
